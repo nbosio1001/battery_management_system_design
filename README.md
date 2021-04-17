a999569be5a6db97e71843d4fa217a0aef93b588
nbosio1001 <n.bosio@yahoo.com>
2021-04-17 00:17:45 -0700

    Making a main file and circuit components file, and updated README.

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # battery_management_system_design
 
 Using PySpice to simulate circuit design based on the All About Circuits "Create an Arduino Controlled Battery Charger" project created by Jason Poel Smith (https://www.allaboutcircuits.com/projects/create-an-arduino-controlled-battery-charger/).
+
+
+battery_charger_design.py will be the main file. Different circuit components will be stored in the circuits_components.py file.
--- battery_charger_design.py
+++ battery_charger_design.py
@@ -1,17 +1,6 @@
-import PySpice.Logging.Logging as Logging
-logger = Logging.setup_logging()
-
-
-from PySpice.Spice.Netlist import Circuit, SubCircuit, SubCircuitFactory
+from circuit_components import ParallelResistor
+from PySpice.Spice.Netlist import Circuit, SubCircuit
 from PySpice.Unit import *
-
-class ParallelResistor(SubCircuitFactory):
-    __name__ = 'parallel_resistor'
-    __nodes__ = ('n1', 'n2')
-    def __init__(self, R1=1@u_Ω, R2=2@u_Ω):
-        super().__init__()
-        self.R(1, 'n1', 'n2', R1)
-        self.R(2, 'n1', 'n2', R2)
 
 circuit = Circuit('Test')
 
